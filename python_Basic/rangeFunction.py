e443b1dd522cc62aec0deb7a5c5fd99e07d1b746
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-08-05 12:53:42 +0530

    range function example

---
+++ python_Basic/rangeFunction.py
@@ -0,0 +1,13 @@
+for i in range(6):
+    print(i)
+
+print(list(range(5,12)))
+
+print(list(range(2,20,2)))
+
+user=["Anuja","Nitin","Sonal","Shrikant"]
+for i in range(len(user)):
+    print(i, user[i])
+
+
+print(sum(range(5)))
