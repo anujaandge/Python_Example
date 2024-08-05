5d781d5d7e211dda31b7dc1915406df2e7bfe7fc
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-08-05 11:31:17 +0530

    new file

---
+++ python_Basic/if_statement.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+x=int(input("Enter the number:"))
+if x<0:
+    x=0
+    print("number become zero")
+elif x==0:
+    print("number is zero")
+else:
+    print("More")
