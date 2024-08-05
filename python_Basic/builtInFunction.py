147ed5f56f06d9b4932ff4cb5ba09413a4a2b419
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-08-05 12:52:57 +0530

    Built In fuction

---
+++ python_Basic/builtInFunction.py
@@ -0,0 +1,11 @@
+print(abs(234.56703))
+#bin used for convert integer to binary
+print(bin(3))
+print(bin(23))
+# char to int and int to char
+print(chr(97),chr(98),chr (68))
+print(ord('A'),ord('a'),ord('B'))
+#enumerate() returns a tuple containg vount()from start which defaults  to 0) and the values obtained from iterating over
+seasons=['Spring','Summer','Fall','Winter']
+print(list(enumerate(seasons)))
+print(list(enumerate(seasons,start=1)))
