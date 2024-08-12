8107fb8e729ef55f158aeecb5a3a98e66389398f
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-08-12 18:54:08 +0530

    scopes in python

---
+++ python_Basic/scopes.py
@@ -0,0 +1,23 @@
+def scope_test():
+    def do_local():
+        spam="local spam"
+    def do_nonlocal():
+        nonlocal spam
+        #print(spam)
+        spam="nonlocal spam"    
+    def do_glbal():
+        global spam
+        print(spam)  
+        spam="global spam"  
+        print(spam)  
+        
+    spam='test spam'
+    do_local()
+    print('Afetr local assignment: ',spam)
+    do_nonlocal()
+    print("After nonlocal assignment: ",spam) 
+    do_glbal()
+    print("After gloabal assignmnet: ",spam)
+    
+scope_test()
+print("global scope: ",spam)       
