cb6872986939499a3615c11745154c34af0b6903
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-08-13 19:09:08 +0530

    nolocal keyword

--- python_Basic/nonlocal.py
+++ python_Basic/nonlocal.py
@@ -1,17 +1,36 @@
-# name='geek'
-# def foo():
-#     name='Geek'
-#     def bar():
-#         nonlocal name
-#         print(name)
-#         name='GEEK'
-#         print(name)
+#The nonlocal keyword is used in nested functions to 
+# reference a variable in the parent function. 
+
+#Advantage
+#It helps in accessing the variable in the upper scope
+#memeory added of variable is reused that saves memory
+
+#Disadvantage
+#can't used to referance global or local variables
+#only be used inside nested structures
+
+
+name='geek'
+def method():
+    def mod():
+        #nonlocal name #compile time error occors
+        print(name)
+        name="Greek"
+
+
+def foo():
+    name='Geek'
+    def bar():
+        nonlocal name
+        print(name)
+        name='GEEK'
+        print(name)
         
-#     bar()
-#     print(name)
+    bar()
+    print(name)
     
-# foo()
-# print(name)        
+foo()
+print(name)        
 
 def fun():
     var1=10
@@ -23,4 +42,4 @@ def fun():
     add()
     print(var1)
 fun()    
-print(var1)    
+   
